8fd07d25a0139bcec607829d88338f7e5c44430c
Lucy <happlb@msoe.edu>
2020-04-19 15:07:25 -0500

    got headings generating

--- htmlMain.py
+++ htmlMain.py
@@ -23,6 +23,8 @@ def create_header_lines():
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <script type="text/javascript" src="script.js"></script>
     <title>twitterBot</title>
 </head>
 <body class = "mainBody">
@@ -40,11 +42,32 @@ def create_body_lines():
     return body
 
 def create_div():
-     return """
-        <div>
+    div="<div>"
+    numOfElements = random.randint(1, 5)
+    for x in range(0, numOfElements):
+        div+=create_elements(random.randint(0, 5))
+    div+="</div>"
+    print(div)
+    return (div)
 
-        </div>
-"""    
+def create_elements(i):
+    switcher={
+                0:create_heading(),
+                1:create_heading(),
+                2:create_heading(),
+                3:create_heading(),
+                4:create_heading(),
+                5:create_heading(),
+             }
+    return switcher.get(i,"""""")
+
+def create_heading():
+    headingValue = str(random.randint(1, 4))
+    heading = "<h" + headingValue+">" + create_content() + "</h" + headingValue+">"
+    return heading
+
+def create_content():
+    return "oh hey im some content"
 
 def create_footer_lines():
     footer = """    </div>
